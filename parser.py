5e20e0394ba5350ebd644ef3d395b4c65ac69b27
Andres del Castillo <110851506+Andres42611@users.noreply.github.com>
2024-02-21 17:02:56 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,15 +1,24 @@
+#!/usr/bin/env python3
+
+
 import argparse
-
-def create_parser():
-    parser = argparse.ArgumentParser(description='Calculate the R(A,B) ratio')
-
-    # Files and mandatory inputs for MSPRIME simulation
-    parser.add_argument('-D', '--direc', type=str, required=False, metavar='SAVING_DIRECTORY', help='Directory to save simulation .tree files')
-    parser.add_argument('-s', '--source', type=str, required=False, metavar='SOURCE_MIGRATION_POP', help='Source population for migration')
-    parser.add_argument('-d', '--dest', type=str, required=False, metavar='DESTINATION_MIGRATION_POP', help='Destination population for migration')
-    parser.add_argument('-C', '--Case', type=str, required=False, metavar='MIGRATION_CASE', help='Migration Case for Study')
     
-    #Files and mandatory inputs for VCF to STRU file conversion
-    parser.add_argument('-P', '--POP', type=str, required=False, metavar='POPULATION_FILE_PATH', help='Path to tab-separated population file')
+def init_Simulation_Parser():
+    parser = argparse.ArgumentParser(description='msprime Simulation parameters')
+    
+    # Files and mandatory inputs for MSPRIME simulation
+    parser.add_argument('-D', '--direc', type=str, required=True, metavar='SAVING_DIRECTORY', help='Directory to save simulation .tree files')
+    parser.add_argument('-s', '--source', type=str, required=True, metavar='SOURCE_MIGRATION_POP', help='Source population for migration')
+    parser.add_argument('-d', '--dest', type=str, required=True, metavar='DESTINATION_MIGRATION_POP', help='Destination population for migration')
+    parser.add_argument('-C', '--Case', type=str, required=True, metavar='MIGRATION_CASE', help='Migration Case for Study')
+    
+    return parser
+    
+def init_VCFtoSTRU_Parser():
+    parser = argparse.ArgumentParser(description='msprime Simulation parameters')
+
+    # Files and mandatory inputs for converting VCF file to .stru file
+    parser.add_argument('-V', '--VCF', type=str, required=True, metavar='VCF_FILE', help='VCF file for conversion')
+    parser.add_argument('-P', '--POP', type=str, required=True, metavar='POPULATION_FILE', help='Population tab-delimited txt file with IDs and Pop IDs')
 
     return parser
