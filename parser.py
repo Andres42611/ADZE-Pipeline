da34427ff17af58abb68bba139e8248ce6c94130
Andres del Castillo <110851506+Andres42611@users.noreply.github.com>
2024-02-21 20:25:13 -0500

    Update parser.py to include ADZEtoCSV.py functionalities

--- parser.py
+++ parser.py
@@ -26,3 +26,14 @@ def init_VCFtoSTRU_Parser():
     parser.add_argument('-P', '--POP', type=str, required=True, metavar='POPULATION_FILE', help='Population tab-delimited txt file with IDs and Pop IDs')
 
     return parser
+
+def init_ADZEtoCSV_Parser():
+    parser = argparse.ArgumentParser(description='ADZE output to .csv parameters')
+
+    # Files and mandatory inputs for converting VCF file to .stru file
+    parser.add_argument('-r', '--rout', type=str, required=True, metavar='RICHNESS_FILE', help='R_OUT File Path')
+    parser.add_argument('-p', '--pout', type=str, required=True, metavar='PRIVATE_RICHNESS_FILE', help='P_OUT File Path')
+    parser.add_argument('-c', '--cout', type=str, required=True, metavar='COMB_RICHNESS_FILE', help='C_OUT File Path')
+    parser.add_argument('-C', '--Case', type=str, required=True, metavar='CLASS', help='Classification label')
+
+    return parser
