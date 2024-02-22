735b1109e57aca491f61df0faeb46621b4cfde45
Andres del Castillo <110851506+Andres42611@users.noreply.github.com>
2024-02-21 20:55:56 -0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -31,7 +31,7 @@ for i in {0..99}; do
     ./ADZE-1.0/adze-1.0 sim_paramfile.txt -f "$1/$4_rep_${i}.stru" -l ${num_loci} -r "$1/$4_rep_${i}richness" -p "$1/$4_rep_${i}private" -o "$1/$4_rep_${i}comb" -pp 0 
 
     # After obtaining our ADZE analysis files, convert the replicate's data to a CSV dataset (making sure to add a _2 to the comb file name)
-    ./ADZEtoCSV.py -r "$1/$4_rep_${i}richness" -p "$1/$4_rep_${i}private" -o "$1/$4_rep_${i}comb_2" -C "$4"
+    ./ADZEtoCSV.py -r "$1/$4_rep_${i}richness" -p "$1/$4_rep_${i}private" -c "$1/$4_rep_${i}comb_2" -C "$4"
     
     # Finally clean the directory to only leave replicate's dataset
     rm "$1/$4_rep_${i}.trees"
